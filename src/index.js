2820f53700a79dd3376f57a659b43b179d92cb72
Jayden Tucker <jaydenrtucker@gmail.com>
2020-10-25 15:31:29 -0700

    The sensors are now queried every ten minutes

--- src/index.js
+++ src/index.js
@@ -58,23 +58,29 @@ app.get("/:id/history", async (req, res) => {
 
 app.listen(PORT, async () => {
   console.log(`Server listening on port ${PORT}!\n`)
-  
-  queries.selectAllSensors(db)
-  .then((response)=>{
-    //Query each sensor
-    response.forEach(sensor => {
-      querySensor(sensor);
-    });
-  })
+
+  //Find and query all sensors every ten minutes
+  const queryTimer = setInterval(()=>{
+    queries.selectAllSensors(db)
+    .then((response)=>{
+
+      //Query each sensor
+      response.forEach(sensor => {
+        querySensor(sensor);
+      });
+    })
+  }, 600000)
 });
 
+//Function that queries a sensor, inserts data if connected, and emails users if co2 levels are to high
 const querySensor = async (sensor)=>{
   console.log(`Connecting to ${sensor.name}'s sensor at ${sensor.url}...\n`)
   
   axios.get(sensor.url, { timeout: 3000 })
   .then((sensorResponse)=>{
     //Insert the response data to the database
-    console.log(`Successfully connected to ${sensor.name}'s sensor at ${sensor.url}. Querying and inserting data...\n`)
+    console.log(`Successfully connected to ${sensor.name}'s sensor at ${sensor.url}. Querying and inserting data...\n`);
+
     queries.insertSensorData(db, {sensors_id: sensor.id, co2: sensorResponse.data.co2, tvoc: sensorResponse.data.tvoc})
     .then((response)=>{
       console.log(`Successfully inserted data: Co2:${sensorResponse.data.co2} Tvoc:${sensorResponse.data.tvoc}\n`)
@@ -85,7 +91,8 @@ const querySensor = async (sensor)=>{
 
     //If the co2 level is to high, alert users
     if(sensorResponse.data.co2 > 200){
-      console.log('Co2 levels to high, alerting users...')
+      console.log('Co2 levels to high, alerting users...');
+
       queries.selectSensorAlerts(db, {sensors_id: 1})
       .then((response)=>{
         //Will email users
@@ -93,10 +100,11 @@ const querySensor = async (sensor)=>{
       })
       .catch((err)=>{
         console.log(err);
-      })
+      });
+
     }
   })
   .catch((err)=>{
-    console.log(`Failed to connect to ${sensor.name}'s sensor at ${sensor.url}\n-----Response:-----\n ${err}\n`)
+    console.log(`Failed to connect to ${sensor.name}'s sensor at ${sensor.url}\n-----Response:-----\n ${err}\n`);
   })
 }
