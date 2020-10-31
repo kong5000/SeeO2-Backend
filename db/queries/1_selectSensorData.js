3632fbcfe430c720855c0cf8a397cd9f16cf9345
Jayden Tucker <jaydenrtucker@gmail.com>
2020-10-30 19:43:43 -0700

    Rolled back the date by a day to get the correct date

--- db/queries/1_selectSensorData.js
+++ db/queries/1_selectSensorData.js
@@ -1,6 +1,6 @@
 const selectSensorData = (db, options) => {
   return db.query(`
-    SELECT *, TO_CHAR(sensor_data.date AT TIME ZONE $2, 'Month, DD, HH12:MI') as date FROM sensors
+    SELECT *, TO_CHAR(sensor_data.date AT TIME ZONE $2 - interval '1 day', 'Month, DD, HH12:MI') as date FROM sensors
     JOIN sensor_data ON sensors.id = sensor_data.sensors_id
     WHERE sensors.id = $1
     ORDER BY sensor_data.date DESC;
