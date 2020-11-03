4d6cbd553bade935c492bf455cc3273fc3f75a39
kong5000 <keith.ong5000@gmail.com>
2020-11-03 14:39:46 -0800

    Add file with permission queries

--- db/seeds/1_sensors.sql
+++ db/seeds/1_sensors.sql
@@ -1,2 +1,9 @@
-INSERT INTO sensors(latitude, longitude, name, url, users_id)
-VALUES (48.449100, -123.397710, 'Keith', 'https://arduinokeith123.loca.lt', 1);
+INSERT INTO sensors(latitude, longitude, name, url)
+VALUES (48.449100, -123.397710, 'Keith', 'https://arduinokeith123.loca.lt');
+
+INSERT INTO sensors(latitude, longitude, name, url)
+VALUES (48.6351727, -123.4148484, 'Mark', 'https://arduinomark123.loca.lt');
+
+INSERT INTO sensors(latitude, longitude, name, url)
+VALUES (48.4501635, -123.3959949, 'Jayden', 'https://arduinojayden123.loca.lt');
+
