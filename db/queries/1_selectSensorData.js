415570ff390fd66962489f3eadb43d6e1dbf2801
Jayden Tucker <jaydenrtucker@gmail.com>
2020-11-03 11:42:22 -0800

    changed the formatting of the date in the selectSensorData query

--- db/queries/1_selectSensorData.js
+++ db/queries/1_selectSensorData.js
@@ -1,6 +1,6 @@
 const selectSensorData = (db, options) => {
   return db.query(`
-    SELECT *, TO_CHAR(sensor_data.date + interval 'seconds ${options.timezoneOffset}', 'Month, DD, HH:MI AM') as date FROM sensors
+    SELECT *, TO_CHAR(sensor_data.date + interval 'seconds ${options.timezoneOffset}', 'Day, Month, DD, HH:MI AM, YYYY') as date FROM sensors
     JOIN sensor_data ON sensors.id = sensor_data.sensors_id
     WHERE sensors.id = $1
     ORDER BY sensor_data.date DESC;
